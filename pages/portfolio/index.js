62969f167d6a8dee0a9434344fba5410ab0457a7
= <=>
2022-08-26 02:48:36 +0100

    start of portfolio page

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,7 +1,113 @@
 export default function Portfolio() {
   return (
     <>
-      <h1>this is the portfolio page</h1>
+      <div className="container">
+        <div className="c-page-heading">
+          <h1 className="c-page-heading__title">Software Portfolio</h1>
+          <p className="c-page-heading__description">
+            A collection of my software related projects, all of which are open
+            source - feel free to re-use any of these projects or even
+            contribute to them :)
+          </p>
+        </div>
+      </div>
+
+      <div className="container animate">
+        <div className="row">
+          <div className="c-blog-card col col-4 col-d-6 col-t-12">
+            <div className="c-blog-card__inner">
+              <div className="c-blog-card__image-wrap">
+                <a className="c-blog-card__image" href="">
+                  <img
+                    loading="lazy"
+                    src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg"
+                  />
+                </a>
+              </div>
+
+              <div className="c-blog-card__content">
+                <div className="c-blog-card__tags-box">
+                  <a className="c-blog-card__tag">NextJS</a>
+
+                  <a className="c-blog-card__tag">Web</a>
+                </div>
+
+                <h2 className="c-blog-card__title">
+                  <a href="">This Site :)</a>
+                </h2>
+
+                <p className="c-blog-card__excerpt">
+                  This site combines a wide variety of tech, such as NextJS, the
+                  Twilio API, and more. Read to checkout my development process
+                  :)
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="c-blog-card col col-4 col-d-6 col-t-12">
+            <div className="c-blog-card__inner">
+              <div className="c-blog-card__image-wrap">
+                <a className="c-blog-card__image" href="">
+                  <img
+                    loading="lazy"
+                    src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg"
+                  />
+                </a>
+              </div>
+
+              <div className="c-blog-card__content">
+                <div className="c-blog-card__tags-box">
+                  <a className="c-blog-card__tag">NextJS</a>
+
+                  <a className="c-blog-card__tag">Web</a>
+                </div>
+
+                <h2 className="c-blog-card__title">
+                  <a href="">This Site :)</a>
+                </h2>
+
+                <p className="c-blog-card__excerpt">
+                  This site combines a wide variety of tech, such as NextJS, the
+                  Twilio API, and more. Read to checkout my development process
+                  :)
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="c-blog-card col col-4 col-d-6 col-t-12">
+            <div className="c-blog-card__inner">
+              <div className="c-blog-card__image-wrap">
+                <a className="c-blog-card__image" href="">
+                  <img
+                    loading="lazy"
+                    src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg"
+                  />
+                </a>
+              </div>
+
+              <div className="c-blog-card__content">
+                <div className="c-blog-card__tags-box">
+                  <a className="c-blog-card__tag">NextJS</a>
+
+                  <a className="c-blog-card__tag">Web</a>
+                </div>
+
+                <h2 className="c-blog-card__title">
+                  <a href="">This Site :)</a>
+                </h2>
+
+                <p className="c-blog-card__excerpt">
+                  This site combines a wide variety of tech, such as NextJS, the
+                  Twilio API, and more. Read to checkout my development process
+                  :)
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
