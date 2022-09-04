15bea0b6c6caa96d8dba7ffcb34ff250b01a6858
= <=>
2022-09-04 20:31:20 +0100

    local tags images

--- components/Home/Projects.js
+++ components/Home/Projects.js
@@ -37,9 +37,9 @@ const Projects = () => {
                   title="This Site"
                   description="This site combines a wide variety of tech, such as NextJS, the Twilio API, and more. Read to checkout my development process :)"
                   tags={[
-                    "https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white",
-                    "https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white",
-                    "https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB",
+                    "/projects/tags/next.svg",
+                    "/projects/tags/vercel.svg",
+                    "/projects/tags/react.svg",
                   ]}
                 />
 
@@ -49,19 +49,19 @@ const Projects = () => {
                   title="Personal Blog"
                   description="This github-pages site was made using the static site generator jekyll - it allows for markdown to HTML conversion resulting in a pleasant creator experience."
                   tags={[
-                    "https://img.shields.io/badge/jekyll-%232b2b2b.svg?style=for-the-badge&logo=jekyll&logoColor=%23cc0808",
-                    "https://img.shields.io/badge/ruby-%23CC342D.svg?style=for-the-badge&logo=ruby&logoColor=white",
+                    "/projects/tags/jekyll.svg",
+                    "/projects/tags/ruby.svg",
                   ]}
                 />
+
                 <Card
-                  imageLink="/projects/portfolio.png"
+                  imageLink="/projects/blog.png"
                   postLink=""
-                  title="This Site"
-                  description="This site combines a wide variety of tech, such as NextJS, the Twilio API, and more. Read to checkout my development process :)"
+                  title="Personal Blog"
+                  description="This github-pages site was made using the static site generator jekyll - it allows for markdown to HTML conversion resulting in a pleasant creator experience."
                   tags={[
-                    "https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white",
-                    "https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white",
-                    "https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB",
+                    "/projects/tags/jekyll.svg",
+                    "/projects/tags/ruby.svg",
                   ]}
                 />
               </div>
